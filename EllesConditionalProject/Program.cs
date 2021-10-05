629eea9b1fdd89de53f87433ad5d413dfa728a29
Emmanuelle-Kirk <wolfeetried@gmail.com>
2021-10-05 09:13:58 -0300

    Initial commit

---
+++ EllesConditionalProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EllesConditionalProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Elle's conditional project");
+
+            Console.ReadKey(true);
+        }
+    }
+}
