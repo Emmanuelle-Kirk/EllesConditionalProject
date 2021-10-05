1a8d4ca98d5a9b5fa6025680e6e5405ca1ce6d7a
Emmanuelle-Kirk <wolfeetried@gmail.com>
2021-10-05 10:00:34 -0300

    Update Program.cs

--- EllesConditionalProject/Program.cs
+++ EllesConditionalProject/Program.cs
@@ -8,9 +8,37 @@ namespace EllesConditionalProject
 {
     class Program
     {
+        static int weapon;
+        static int health = 100;
+        static int weaponPickup;
+
+        static void ShowHud()
+        {
+            Console.WriteLine("<3 <3 <3 <3 <3 <3");
+            Console.WriteLine("Health: " + health);
+            Console.WriteLine("Weapon: ");
+            Console.WriteLine("<3 <3 <3 <3 <3 <3");
+        }
+        static void TakeDamage(int damage)
+        {
+            health = health - damage;
+        }
+        static void HealthUp(int hp)
+        {
+            health = health + hp;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Elle's conditional project");
+            if (health < 0)
+            {
+                health = 0;
+            }
+            if (health > 100)
+            {
+                health = 100;
+            }
+            ShowHud();
 
             Console.ReadKey(true);
         }
