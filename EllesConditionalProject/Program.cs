0fe42b26763239676bf8e1a5c5316208ed77ddc6
Emmanuelle-Kirk <wolfeetried@gmail.com>
2021-10-05 10:49:54 -0300

    Update Program.cs

--- EllesConditionalProject/Program.cs
+++ EllesConditionalProject/Program.cs
@@ -8,28 +8,64 @@ namespace EllesConditionalProject
 {
     class Program
     {
-        static int weapon;
         static int health = 100;
-        static int weaponPickup;
+        static int weapon;
+
+        static void Weapon()
+        {
+            Console.WriteLine("Weapon: ");
+            if (weapon == 0)
+            {
+                Console.WriteLine("Pistol");
+            }
+            if (weapon == 1)
+            {
+                Console.WriteLine("Shotgun");
+            }
+            if (weapon == 2)
+            {
+                Console.WriteLine("Spreader");
+            }
+            if (weapon == 3)
+            {
+                Console.WriteLine("Laser");
+            }
+            if (weapon == 4)
+            {
+                Console.WriteLine("Sniper");
+            }
+            if (weapon == 5)
+            {
+                Console.WriteLine("BFG");
+            }
+
+            if (weapon < 0)
+            {
+                weapon = 0;
+            }
+            if (weapon > 5)
+            {
+                weapon = 5;
+            }
+        }
 
         static void ShowHud()
         {
             Console.WriteLine("<3 <3 <3 <3 <3 <3");
-            Console.WriteLine("Health: " + health);
-            Console.WriteLine("Weapon: ");
+            Console.WriteLine();
+            Console.WriteLine("Health: ");
+            Console.WriteLine(+ health);
+            Console.WriteLine();
+            Weapon();
+            Console.WriteLine();
             Console.WriteLine("<3 <3 <3 <3 <3 <3");
+            Console.WriteLine();
         }
         static void TakeDamage(int damage)
         {
             health = health - damage;
-        }
-        static void HealthUp(int hp)
-        {
-            health = health + hp;
-        }
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Elle's conditional project");
+            Console.WriteLine("Owch! You were hit!");
+            Console.WriteLine();
             if (health < 0)
             {
                 health = 0;
@@ -38,6 +74,41 @@ namespace EllesConditionalProject
             {
                 health = 100;
             }
+        }
+        static void HealthUp(int hp)
+        {
+            health = health + hp;
+            Console.WriteLine("Nice! You picked up a stimpak!");
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+           
+            ShowHud();
+
+            TakeDamage(25);
+
+            ShowHud();
+
+            HealthUp(25);
+
+            ShowHud();
+
+            TakeDamage(50);
+
+            weapon = weapon + 1;
+
+            Console.WriteLine("You picked up a new weapon!");
+            Console.WriteLine();
+
+            ShowHud();
+
+            TakeDamage(25);
+
+            ShowHud();
+
+            TakeDamage(50);//killing blow
+
             ShowHud();
 
             Console.ReadKey(true);
